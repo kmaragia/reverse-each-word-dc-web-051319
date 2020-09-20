6816a441e322f8e78a747e53608f237a7d15489b
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-20 14:06:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(str)
   arr.each {|x| new_arr<< x.reverse} 
   return new_arr.join(" ")
 end
+
+def reverse_each_word(str)
+  arr = str.split()
+  arr.each {|x| puts x.reverse} 
+  return new_arr.join(" ")
+end
